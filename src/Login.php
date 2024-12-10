dfcff827e148781cdaa62bb6cfae25dfcc43fee9
Anthem456789 <115545512+Anthem456789@users.noreply.github.com>
2024-12-10 22:50:11 +0100

    Add files via upload

---
+++ src/Login.php
@@ -0,0 +1,54 @@
+<?php
+
+include '../Devops/SetUpDb.php'; 
+
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title> Login </title>
+    <link rel="stylesheet" type="text/css" href="Grafica/style.css?<?php echo time(); ?>">
+</head>
+
+<body>
+    </div class="container" id="container">
+    <form action="BackBone_Phoenix/CheckLogin.php" method="POST">
+        <h2>Bentornato</h2>
+        <!-- Get è una richiesta o un invio dati fatta al server web o al pc stesso, dove i parametri sono inviati tramite URL  -->
+        <?php if(isset($_GET['error'])) { ?>
+        <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+
+        <?php
+ if(isset($_GET['success'])) { ?>
+        <p class="success"><?php echo $_GET['success']; ?></p>
+        <?php } ?>
+
+        <label>Email</label>
+        <input type="text" name="email" placeholder="Email" value=<?php 
+  if (!empty($_COOKIE['user'])) {
+    echo $_COOKIE['user'];
+  }
+    ?>><br>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password" value=<?php 
+  if (!empty($_COOKIE['userpass'])) {
+    echo $_COOKIE['userpass'];
+  }
+    ?>><br>
+
+        <a class="register" href="SignUP.php">Registrati</a>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+    </div>
+
+</body>
+
+</html>
---
+++ src/SignUp.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+
+include '../Devops/SetUpDb.php'; 
+
+?>
+
+<!DOCTYPE HTML>  
+<html>
+  <head> 
+   <title> REGISTRAZIONE </title>
+   <link rel="stylesheet" type="text/css" href="Grafica/style.css?<?php echo time(); ?>">
+  </head>
+ 
+<body>
+ 
+
+<form action="BackBone_Phoenix/CheckSignUp.php" method="POST">
+<h2>Benvenuto</h2>
+<?php if(isset($_GET['error'])) { ?>
+  <p class="error"><?php echo $_GET['error']; ?></p>
+<?php } ?>
+
+<label>Nome</label>
+  <input type= "text" name= "nome" placeholder="Nome" value = 
+  <?php 
+  if (!empty($_SESSION['nome'])) {
+    echo $_SESSION['nome'];
+  }
+    ?>><br>
+
+<label>Cognome</label>
+<input type= "text" name= "cognome" placeholder="Cognome" value = 
+  <?php 
+  if (!empty($_SESSION["cognome"])) {
+    echo $_SESSION["cognome"];
+  }
+    ?>><br>
+
+<label>Data Di Nascita</label>
+<input type= "text" name= "data_nascita" placeholder="Data di nascita" value = 
+  <?php 
+  if (!empty($_SESSION["data_nascita"])) {
+    echo $_SESSION["data_nascita"];
+  }
+    ?>><br>
+
+<label>Codice Fiscale</label>
+<input type= "text" name= "codiceFiscale" placeholder="Codice Fiscale" value = 
+  <?php 
+  if (!empty($_SESSION["codiceFiscale"])) {
+    echo $_SESSION["codiceFiscale"];
+  }
+    ?>><br>
+
+<label>Email</label>
+<input type= "text" name= "email" placeholder="Email" value = 
+  <?php 
+  if (!empty( $_SESSION["em"])) {
+    echo  $_SESSION["em"];
+  }
+    ?>><br>
+
+<label>Password</label>
+<input type= "password" name= "password" placeholder="Password(Inserisci almeno un numero, una maiuscola) - Min: 7 caratteri" value = 
+  <?php 
+  if (!empty($_SESSION["pass"])) {
+   echo $_SESSION["pass"];
+  }
+    ?>><br>
+
+<label>Ripeti Password</label>
+<input type= "password" name= "Re_password" placeholder="Ripeti Password" value = 
+  <?php 
+  if (!empty($_SESSION["Re_password"])) {
+    echo $_SESSION["Re_password"];
+  }
+    ?>><br>
+
+  <div class ="CategoriaBox">
+
+   <label id="lab">categoria</label>
+   <input  id="Cat" type="radio" name="gender" value="1"><p>Medico</p>
+   <input id="Cat" type="radio" name="gender" value="2"><p>Infermiere</p>
+   <input id="Cat" type="radio" name="gender" value="3"><p>Paziente</p>
+
+  </div>
+
+<a class="register" href="Login.php">Hai già un account?</a>  
+  
+<button type="submit">Registrati</button>
+
+  </form>
+
+  <?php 
+
+   ?>
+
+</body>
+</html>
