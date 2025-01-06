f3086dc7ef94cb43847d0c8a9dd432f9e9be287d
Anthem456789 <115545512+Anthem456789@users.noreply.github.com>
2025-01-06 22:50:22 +0100

    Update SignUp.php

--- src/SignUp.php
+++ src/SignUp.php
@@ -3,7 +3,7 @@
 <html>
   <head> 
    <title> REGISTRAZIONE </title>
-   <link rel="stylesheet" type="text/css" href="Grafica/style.css?<?php echo time(); ?>">
+   <link rel="stylesheet" type="text/css" href="BackBone_Phoenix/GraficaForm.css?<?php echo time(); ?>">
   </head>
  
 <body>
@@ -18,56 +18,56 @@
 <label>Nome</label>
   <input type= "text" name= "nome" placeholder="Nome" value = 
   <?php 
-  if (!empty($_SESSION['nome'])) {
-    echo $_SESSION['nome'];
+  if (!empty($_COOKIE['nome'])) {
+    echo $_COOKIE['nome'];
   }
     ?>><br>
 
 <label>Cognome</label>
 <input type= "text" name= "cognome" placeholder="Cognome" value = 
   <?php 
-  if (!empty($_SESSION["cognome"])) {
-    echo $_SESSION["cognome"];
+  if (!empty($_COOKIE['cognome'])) {
+    echo  $_COOKIE['cognome'];
   }
     ?>><br>
 
 <label>Data Di Nascita</label>
 <input type= "text" name= "data_nascita" placeholder="Data di nascita" value = 
   <?php 
-  if (!empty($_SESSION["data_nascita"])) {
-    echo $_SESSION["data_nascita"];
+  if (!empty($_COOKIE['data'])) {
+    echo  $_COOKIE['data'];
   }
     ?>><br>
 
 <label>Codice Fiscale</label>
 <input type= "text" name= "codiceFiscale" placeholder="Codice Fiscale" value = 
   <?php 
-  if (!empty($_SESSION["codiceFiscale"])) {
-    echo $_SESSION["codiceFiscale"];
+  if (!empty($_COOKIE['codiceFiscale'])) {
+    echo  $_COOKIE['codiceFiscale'];
   }
     ?>><br>
 
 <label>Email</label>
 <input type= "text" name= "email" placeholder="Email" value = 
   <?php 
-  if (!empty( $_SESSION["em"])) {
-    echo  $_SESSION["em"];
+  if (!empty( $_COOKIE['email'])) {
+    echo   $_COOKIE['email'];
   }
     ?>><br>
 
 <label>Password</label>
 <input type= "password" name= "password" placeholder="Password(Inserisci almeno un numero, una maiuscola) - Min: 7 caratteri" value = 
   <?php 
-  if (!empty($_SESSION["pass"])) {
-   echo $_SESSION["pass"];
+  if (!empty($_COOKIE['userpass'])) {
+   echo  $_COOKIE['userpass'];
   }
     ?>><br>
 
 <label>Ripeti Password</label>
 <input type= "password" name= "Re_password" placeholder="Ripeti Password" value = 
   <?php 
-  if (!empty($_SESSION["Re_password"])) {
-    echo $_SESSION["Re_password"];
+  if (!empty($_COOKIE['Re_password'])) {
+    echo  $_COOKIE['Re_password'];
   }
     ?>><br>
 
